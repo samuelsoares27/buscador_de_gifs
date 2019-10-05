91e2653ef62254f40b21c1c4d32a0b6771ef58b9
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2019-10-05 17:44:31 -0300

    Initial commit

---
+++ search_gif/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:search_gif/ui/gif_page.dart';
+import 'package:search_gif/ui/home_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
